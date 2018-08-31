e98e0a7204e0a18ce568398944f370474eeb4b4d
petertrr <peter.trifanov@mail.ru>
2018-08-31 15:21:29 +0300

    Initial project structure

---
+++ src/test/java/com/db/ClientControllerTest.java
@@ -0,0 +1,14 @@
+package com.db;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ClientControllerTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
