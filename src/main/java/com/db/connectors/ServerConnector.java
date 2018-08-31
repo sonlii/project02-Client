59ba1207f66b5a8c11c95af498a72f9a8155a4b6
lorddux <lorddux@inbox.ru>
2018-08-31 16:21:17 +0300

    Add gitignore

--- src/main/java/com/db/connectors/ServerConnector.java
+++ src/main/java/com/db/connectors/ServerConnector.java
@@ -3,10 +3,11 @@ package com.db.connectors;
 import com.db.utils.sctructures.Message;
 
 import java.util.Collection;
+import java.util.Date;
 
 public interface ServerConnector {
     Collection<Message> getMessages(long timestamp);
     Collection<Message> getHistory();
-    long getServerTime();
+    Date getServerTime();
     int sendMessage(Message message);
 }
