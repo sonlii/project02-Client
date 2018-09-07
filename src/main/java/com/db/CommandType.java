38f08692a1081153f456a4c3deaf0cba53e9d7bf
petertrr <peter.trifanov@mail.ru>
2018-09-07 12:57:59 +0300

    Fixed some issues + added documetation.

--- src/main/java/com/db/CommandType.java
+++ src/main/java/com/db/CommandType.java
@@ -2,7 +2,11 @@ package com.db;
 
 /**
  * Enum representing available types of commands
- * Currently available commands: send, get histroy, quit form chat
+ * Currently available commands:
+ * SND: send message,
+ * HIST: get histroy,
+ * QUIT: quit form chat
+ * CHID: change current user login
  */
 public  enum CommandType {
     SND,
