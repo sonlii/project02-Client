225af6df1c69d9e54cbbbf68d25bba3d92c5acf2
petertrr <peter.trifanov@mail.ru>
2018-09-07 11:15:02 +0300

    Removed unused imports.

--- src/main/java/com/db/ServerApp.java
+++ src/main/java/com/db/ServerApp.java
@@ -1,17 +1,10 @@
 package com.db;
 
-import com.db.client.ClientController;
-import com.db.commands.CommandFactory;
-import com.db.connectors.ServerConnector;
 import com.db.server.Server;
 import com.db.server.persistance.FileRepository;
-import com.db.utils.ConsoleParser;
 import com.db.utils.JsonSerializer;
-import com.db.utils.decorators.ConsoleDecorator;
 
 import java.io.*;
-
-import static java.lang.Thread.sleep;
 
 public class ServerApp {
     public static void main(String[] args) throws IOException, InterruptedException {
